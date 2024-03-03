c1b63145e1d6c33b1c64693803be078b6b3ea803
Michela <micheladebellis@gmail.com>
2024-03-03 13:38:29 +0100

    Add react components folder and all the files inside it (empty files). In Header.js added styling and the basic elements (button to add new books to the list and the books' counter)

---
+++ bookshelfreactapp/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = ({toggleModal, nbOfBooks}) => {
+  return (
+    <header className="header">
+        <div className="container">
+            <h3>Book List ({nbOfBooks})</h3>
+            <button onClick={() => toogleModal(true)} className='btn'>
+                <i className='bi bi-flower2'></i> Add new Book
+            </button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
